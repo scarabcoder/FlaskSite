1f57a5b6f7974b6dd5df18c5066fe95f388e2d8d
scarabcoder <scarabcoder@gmail.com>
2016-02-05 09:40:03 -0800

    Up

--- app/login.py
+++ app/login.py
@@ -7,6 +7,14 @@ from .forms import LoginForm
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        error = False
+        return redirect('/index')
+    else:
+        error = True
     return render_template('login.html',
                            title='Sign In',
+                           error = error,
                            form=form)
